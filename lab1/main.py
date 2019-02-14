b0d781480f616890578c54aa07cc09b01254a353
Gustav Schoelin <scholin.gustav@gmail.com>
2019-02-14 17:26:50 +0100

    Small fix

--- lab1/main.py
+++ lab1/main.py
@@ -1,9 +1,9 @@
-from lab1.board import Turn
+from lab1.board import Color
 from lab1.player import HumanPlayer
 from lab1.game import Game
 
-player1 = HumanPlayer(Turn.WHITE)
-player2 = HumanPlayer(Turn.BLACK)
+player1 = HumanPlayer(Color.WHITE)
+player2 = HumanPlayer(Color.BLACK)
 
 game = Game(player1, player2)
 game.start()
