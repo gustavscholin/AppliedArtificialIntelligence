3b9b20ed95849905b2e38c1278b4ef03eff60db0
NoahKSolomon <NoahKSolomon@gmail.com>
2019-02-27 17:04:07 +0100

    Added Linear Regression

---
+++ lab3/linear_regression.py
@@ -0,0 +1,78 @@
+#
+# Perform the linear regression for the number of characters vs numbers of a's in the text
+# Author: Noah Solomon, Gustav Scholin
+#
+
+
+import lab3.datasets as ds
+import copy
+
+
+# ----------------
+# Helper Functions
+# ----------------
+
+
+# Normalize a list by its largest value
+# Parameters: List to normalize
+# Return: Normalized list
+def normalize_data(in_list):
+    max_val = in_list[0]
+    for i in range(0, len(in_list)):
+        if max_val < in_list[i]:
+            max_val = in_list[i]
+    ret_list = copy.deepcopy(in_list)
+    for i in range(0, len(ret_list)):
+        ret_list[i] = ret_list[i] / max_val
+    return ret_list
+
+
+# Function to find the best fit line using batch gradient descent
+# Parameters: list of chars, list of as, and learning rate
+# Return: Weights w0 and w1 in the line y = w0 + w1*x
+def fit_line_batch(char_list, a_list, rate):
+    w0, w1 = 0.5, 0.5
+
+    # Normalize data to range [0,1]
+    char_list_n = normalize_data(char_list)
+    a_list_n = normalize_data(a_list)
+
+    # Start gradient descent
+    for i in range(0, 500):
+        w0_loss = 0
+        w1_loss = 0
+
+        # Calculate the loss for w0 and w1
+        for j in range(0, len(a_list)):
+            w0_loss = w0_loss + (char_list_n[j] - (w0 + w1 * a_list_n[j]))
+            w1_loss = w1_loss + (char_list_n[j] - (w0 + w1 * a_list_n[j]))
+
+        w0 = w0 + l_rate * w0_loss
+        w1 = w0 + l_rate * w1_loss
+
+    return w0, w1
+
+
+# Function to find the best fit line using stochastic gradient descent
+# Parameters: number of total characters, number of as, and learning rate
+# Return: Weights w0 and w1 in the line y = w0 + w1*x
+def fit_line_stochastic(char_list, a_list, rate):
+    w0, w1 = 0.5, 0.5
+    return w0, w1
+
+
+# ------------------------------
+# Linear Regression Script Start
+# ------------------------------
+
+# Retrieve data
+en_chars, en_as = ds.get_data_en()
+fr_chars, fr_as = ds.get_data_fr()
+
+l_rate = 1  # Learning rate for the gradient descent
+
+en_w0, en_w1 = fit_line_batch(en_chars, en_as, l_rate)
+fr_w0, fr_w1 = fit_line_batch(fr_chars, fr_as, l_rate)
+
+
+
